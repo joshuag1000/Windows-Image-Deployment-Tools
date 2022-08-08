32e4bae95282d3814fd98addbfa6de210925fdf5
Joshua Glass <28513051+joshuag1000@users.noreply.github.com>
2022-08-08 22:25:20 +0100

    Minor rewrite to ensure script behaves as expected
    It is worth noting that there are no checks in place to check to see if the value entered for the image is actually correct.
    This may come in the future

--- WinPE/Scripts/Deployment/Build.bat
+++ WinPE/Scripts/Deployment/Build.bat
@@ -8,11 +8,7 @@ for /f "eol=: delims=" %%F in ('dir /b \*.wim') do (
 )
 if %folderCnt% EQU 0 (
     echo No Wim files were found.
-	set /P c=Would you like to capture the existing image on this device [Y/N]? 
-    if /I "%c%" EQU "N" goto :EOF
-    if /I "%c%" EQU "n" goto :EOF
-    call CaptureImage.bat
-    goto :EOF
+    goto :CaptureQuestion
 )
 if %folderCnt% EQU 1 (
     echo Only one wim file detected selecting file.
@@ -30,9 +26,17 @@ if not "!folder%selection%!"=="" (
     call ApplyRecovery.bat
 ) else (
     echo No Wim files were found.
-	set /P c=Would you like to capture the existing image on this device [Y/N]? 
-    if /I "%c%" EQU "N" goto :EOF
-    if /I "%c%" EQU "n" goto :EOF
-    call CaptureImage.bat
-    goto :EOF
+    goto :CaptureQuestion
 )
+goto :EOF
+
+:CaptureQuestion
+set /P c=Would you like to capture the existing image on this device [Y/N]? 
+if /I "%c%" EQU "N" goto :EOF
+if /I "%c%" EQU "n" goto :EOF
+if /I "%c%" EQU "Y" goto :CaptureImage
+if /I "%c%" EQU "y" goto :CaptureImage
+goto :CaptureQuestion
+
+:CaptureImage
+call CaptureImage.bat
