b872b247a7601854b40588e2229eaf28b981f65f
Joshua Glass <joshuag1000@outlook.com>
2023-04-08 05:28:02 +0100

    Begin designing elements from the old scripts.

--- WIDT-GUI-SRC/WIDT-GUI-Setup/StartForm.Designer.vb
+++ WIDT-GUI-SRC/WIDT-GUI-Setup/StartForm.Designer.vb
@@ -27,7 +27,24 @@ Partial Class StartForm
         ToolStripSeparator1 = New ToolStripSeparator()
         QuitToolStripMenuItem = New ToolStripMenuItem()
         TableLayoutPanel1 = New TableLayoutPanel()
+        FlowLayoutPanel1 = New FlowLayoutPanel()
+        Label1 = New Label()
+        Label2 = New Label()
+        FlowLayoutPanel3 = New FlowLayoutPanel()
+        TableLayoutPanel2 = New TableLayoutPanel()
+        Label3 = New Label()
+        FlowLayoutPanel2 = New FlowLayoutPanel()
+        Label4 = New Label()
+        FlowLayoutPanel4 = New FlowLayoutPanel()
+        ChkWinPEStatus = New CheckBox()
+        btnWinPESetup = New Button()
+        Button1 = New Button()
         MenuStrip1.SuspendLayout()
+        TableLayoutPanel1.SuspendLayout()
+        FlowLayoutPanel1.SuspendLayout()
+        TableLayoutPanel2.SuspendLayout()
+        FlowLayoutPanel2.SuspendLayout()
+        FlowLayoutPanel4.SuspendLayout()
         SuspendLayout()
         ' 
         ' MenuStrip1
@@ -63,13 +80,144 @@ Partial Class StartForm
         TableLayoutPanel1.ColumnCount = 2
         TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
         TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
-        TableLayoutPanel1.Location = New Point(202, 183)
+        TableLayoutPanel1.Controls.Add(FlowLayoutPanel1, 0, 0)
+        TableLayoutPanel1.Controls.Add(FlowLayoutPanel3, 1, 1)
+        TableLayoutPanel1.Controls.Add(TableLayoutPanel2, 0, 1)
+        TableLayoutPanel1.Dock = DockStyle.Fill
+        TableLayoutPanel1.Location = New Point(0, 24)
         TableLayoutPanel1.Name = "TableLayoutPanel1"
         TableLayoutPanel1.RowCount = 2
-        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 50F))
-        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 50F))
-        TableLayoutPanel1.Size = New Size(200, 100)
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 62F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 100F))
+        TableLayoutPanel1.Size = New Size(800, 426)
         TableLayoutPanel1.TabIndex = 1
+        ' 
+        ' FlowLayoutPanel1
+        ' 
+        FlowLayoutPanel1.Controls.Add(Label1)
+        FlowLayoutPanel1.Controls.Add(Label2)
+        FlowLayoutPanel1.Dock = DockStyle.Fill
+        FlowLayoutPanel1.Location = New Point(3, 3)
+        FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        FlowLayoutPanel1.Size = New Size(394, 56)
+        FlowLayoutPanel1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(3, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(355, 30)
+        Label1.TabIndex = 1
+        Label1.Text = "Windows Image Deployment Tools"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(3, 30)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(225, 21)
+        Label2.TabIndex = 2
+        Label2.Text = "Setup and Configuration Utility"
+        ' 
+        ' FlowLayoutPanel3
+        ' 
+        FlowLayoutPanel3.Dock = DockStyle.Fill
+        FlowLayoutPanel3.FlowDirection = FlowDirection.TopDown
+        FlowLayoutPanel3.Location = New Point(403, 65)
+        FlowLayoutPanel3.Name = "FlowLayoutPanel3"
+        FlowLayoutPanel3.Size = New Size(394, 358)
+        FlowLayoutPanel3.TabIndex = 2
+        ' 
+        ' TableLayoutPanel2
+        ' 
+        TableLayoutPanel2.ColumnCount = 1
+        TableLayoutPanel2.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 100F))
+        TableLayoutPanel2.Controls.Add(Label3, 0, 0)
+        TableLayoutPanel2.Controls.Add(FlowLayoutPanel2, 0, 1)
+        TableLayoutPanel2.Dock = DockStyle.Fill
+        TableLayoutPanel2.Location = New Point(3, 65)
+        TableLayoutPanel2.Name = "TableLayoutPanel2"
+        TableLayoutPanel2.RowCount = 2
+        TableLayoutPanel2.RowStyles.Add(New RowStyle(SizeType.Absolute, 22F))
+        TableLayoutPanel2.RowStyles.Add(New RowStyle(SizeType.Percent, 100F))
+        TableLayoutPanel2.Size = New Size(394, 358)
+        TableLayoutPanel2.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(3, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(209, 21)
+        Label3.TabIndex = 1
+        Label3.Text = "Install WIDT To USB or ISO"
+        ' 
+        ' FlowLayoutPanel2
+        ' 
+        FlowLayoutPanel2.Controls.Add(Label4)
+        FlowLayoutPanel2.Controls.Add(FlowLayoutPanel4)
+        FlowLayoutPanel2.Dock = DockStyle.Fill
+        FlowLayoutPanel2.FlowDirection = FlowDirection.TopDown
+        FlowLayoutPanel2.Location = New Point(3, 25)
+        FlowLayoutPanel2.Name = "FlowLayoutPanel2"
+        FlowLayoutPanel2.Size = New Size(388, 330)
+        FlowLayoutPanel2.TabIndex = 2
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(3, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(98, 20)
+        Label4.TabIndex = 0
+        Label4.Text = "WinPE Status:"
+        ' 
+        ' FlowLayoutPanel4
+        ' 
+        FlowLayoutPanel4.AutoSize = True
+        FlowLayoutPanel4.Controls.Add(ChkWinPEStatus)
+        FlowLayoutPanel4.Controls.Add(btnWinPESetup)
+        FlowLayoutPanel4.Controls.Add(Button1)
+        FlowLayoutPanel4.Dock = DockStyle.Top
+        FlowLayoutPanel4.Location = New Point(3, 23)
+        FlowLayoutPanel4.Name = "FlowLayoutPanel4"
+        FlowLayoutPanel4.Size = New Size(266, 30)
+        FlowLayoutPanel4.TabIndex = 3
+        ' 
+        ' ChkWinPEStatus
+        ' 
+        ChkWinPEStatus.AutoCheck = False
+        ChkWinPEStatus.AutoSize = True
+        ChkWinPEStatus.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point)
+        ChkWinPEStatus.Location = New Point(3, 3)
+        ChkWinPEStatus.Name = "ChkWinPEStatus"
+        ChkWinPEStatus.Size = New Size(98, 24)
+        ChkWinPEStatus.TabIndex = 2
+        ChkWinPEStatus.Text = "Not Found"
+        ChkWinPEStatus.UseVisualStyleBackColor = True
+        ' 
+        ' btnWinPESetup
+        ' 
+        btnWinPESetup.Location = New Point(107, 3)
+        btnWinPESetup.Name = "btnWinPESetup"
+        btnWinPESetup.Size = New Size(75, 23)
+        btnWinPESetup.TabIndex = 3
+        btnWinPESetup.Text = "Setup"
+        btnWinPESetup.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(188, 3)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Locate..."
+        Button1.UseVisualStyleBackColor = True
         ' 
         ' StartForm
         ' 
@@ -83,6 +231,15 @@ Partial Class StartForm
         Text = "Setup - Windows Image Deployment Tools"
         MenuStrip1.ResumeLayout(False)
         MenuStrip1.PerformLayout()
+        TableLayoutPanel1.ResumeLayout(False)
+        FlowLayoutPanel1.ResumeLayout(False)
+        FlowLayoutPanel1.PerformLayout()
+        TableLayoutPanel2.ResumeLayout(False)
+        TableLayoutPanel2.PerformLayout()
+        FlowLayoutPanel2.ResumeLayout(False)
+        FlowLayoutPanel2.PerformLayout()
+        FlowLayoutPanel4.ResumeLayout(False)
+        FlowLayoutPanel4.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -92,4 +249,16 @@ Partial Class StartForm
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
     Friend WithEvents QuitToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FlowLayoutPanel3 As FlowLayoutPanel
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents FlowLayoutPanel2 As FlowLayoutPanel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ChkWinPEStatus As CheckBox
+    Friend WithEvents FlowLayoutPanel4 As FlowLayoutPanel
+    Friend WithEvents btnWinPESetup As Button
+    Friend WithEvents Button1 As Button
 End Class
--- WIDT-GUI-SRC/WIDT-GUI-Setup/StartForm.vb
+++ WIDT-GUI-SRC/WIDT-GUI-Setup/StartForm.vb
@@ -1,4 +1,9 @@
-﻿Public Class StartForm
+﻿Imports System.IO.DriveInfo
+Imports System.IO
+Public Class StartForm
+
+    Dim WinPEPath As String = ""
+
     Private Sub StartForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -6,4 +11,55 @@
     Private Sub QuitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles QuitToolStripMenuItem.Click
         Me.Close()
     End Sub
+
+    Private Sub btnWinPESetup_Click(sender As Object, e As EventArgs) Handles btnWinPESetup.Click
+        Dim ConfigureDialog As DialogResult = ConfigurePEDialog.ShowDialog()
+        If ConfigureDialog = DialogResult.OK Then
+            WinPEPath = ConfigurePEDialog.txtWinPEPath.Text + "\WIDT_WinPE_amd64"
+            Dim IncludeDuplicateMagic As Boolean = ConfigurePEDialog.chkDupMagic.Checked
+            ConfigurePEDialog.Close()
+            ProgressDialog.Show()
+            Dim ADKCommandLine As String = "call ""C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"" && "
+
+            ' check if folder exists and delete it if ok
+            If Directory.Exists(WinPEPath) Then
+                If MessageBox.Show("The folder WIDT_WinPE_amd64 already exists in the folder you selected. Is it ok if it is deleted?", "Folder Exists", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = DialogResult.Yes Then
+                    Directory.Delete(WinPEPath, True)
+                End If
+            End If
+
+            ' CopyPE Command - Setup the command to run
+            ProgressDialog.SetLabelText("Copying WinPE to directory.")
+            If RunCmdCommand(ADKCommandLine + "call copype amd64 " + WinPEPath) Then Return
+            ProgressDialog.SetProgressBarAmount(10)
+
+
+            ProgressDialog.Close()
+        End If
+    End Sub
+
+    Private Function RunCmdCommand(ByVal CommandToRun As String) As Boolean
+        ' Define command to run
+        Dim CommandDefinition As System.Diagnostics.ProcessStartInfo = New System.Diagnostics.ProcessStartInfo("cmd", "/c " + CommandToRun) With {
+            .RedirectStandardOutput = True,
+            .UseShellExecute = False,
+            .CreateNoWindow = True
+        }
+        ' Define the process
+        Dim CopyPEProc As System.Diagnostics.Process = New System.Diagnostics.Process With {
+            .StartInfo = CommandDefinition
+        }
+        CopyPEProc.Start() ' Start the process
+        Dim CmdCopyPEresult As String = CopyPEProc.StandardOutput.ReadToEnd()
+        ' Check for errors
+        If CopyPEProc.ExitCode <> 0 Then
+            Dim errorMessageresult As DialogResult = MessageBox.Show("An Unexpected error occured. Would you like to view the log", "Unexpected Error", MessageBoxButtons.YesNo, MessageBoxIcon.Error)
+            If errorMessageresult = DialogResult.Yes Then
+                MessageBox.Show(CmdCopyPEresult, "Error Log", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+            Return True
+        End If
+        Return False
+    End Function
+
 End Class
